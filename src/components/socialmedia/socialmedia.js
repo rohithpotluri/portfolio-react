5473f2de47fa1927d9f7efc7daec319231dc7989
rohith potluri <rohithpotluri234@gmail.com>
2021-03-09 16:54:28 +0530

    aws-code1

--- src/components/socialmedia/socialmedia.js
+++ src/components/socialmedia/socialmedia.js
@@ -6,25 +6,28 @@ function Socialmedia() {
       <div className="social-menu">
         <div>
           {" "}
-          <a href="https://github.com/rohithpotluri">
+          <a href="https://github.com/rohithpotluri" target="_blank">
             <i className="fa fa-github"></i>
           </a>
         </div>
         <div>
           {" "}
-          <a href="https://www.quora.com/profile/Rohith-Potluri-1">
+          <a
+            href="https://www.quora.com/profile/Rohith-Potluri-1"
+            target="_blank"
+          >
             <i className="fa fa-quora"></i>
           </a>
         </div>
         <div>
           {" "}
-          <a href="https://www.instagram.com/rohithpotluri/">
+          <a href="https://www.instagram.com/rohithpotluri/" target="_blank">
             <i className="fa fa-instagram"></i>
           </a>
         </div>
         <div>
           {" "}
-          <a href="https://www.linkedin.com/in/rohithpotluri/">
+          <a href="https://www.linkedin.com/in/rohithpotluri/" target="_blank">
             <i class="fa fa-linkedin"></i>
           </a>
         </div>
